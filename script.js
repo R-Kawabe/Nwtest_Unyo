e49fb93e4220e60ae04d35c6cd6f285f5a79782e
R-Kawabe <dragontree7@gmail.com>
2021-07-27 00:38:48 +0900

    debug error

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 async function runFunctionApp(){
     const selectTag = document.selectTag.unyoTag;
-    const selectValue = selectTag.selectedIndex;
+    const selectIndex = selectTag.selectedIndex;
+    const selectValue = selectTag.options[selectIndex].value
 
-    const url = "https://nwtest202107.azurewebsites.net/api/HttpTrigger1?code=Pp8lXzXoVdz03spR6hZ6nv0A4jkpHEOM1aIUakueEQqJqjQKYIjHWw=="
-    const bodyData = '{"unyoTag": '+selectValue+'}'
+    const code = "Pp8lXzXoVdz03spR6hZ6nv0A4jkpHEOM1aIUakueEQqJqjQKYIjHWw==";
+    const url = `https://nwtest202107.azurewebsites.net/api/HttpTrigger1?code=${code}`
+    const bodyData = {
+        unyoTag: `${selectValue}`
+    };
     
-    var result = await fetch(url, {
-        method:"POST",
-        body:JSON.stringify(bodyData)
-    });
-
-    document.getElementById("resultArea").innerHTML = result;
+    await fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.blob();
+    })
+    //.then(data => document.getElementById("resultArea").innerHTML = data)
+    .catch(error => console.log(error));
 }
